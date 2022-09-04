1598c2017368c3491ef0581a75f5a727f4810529
Shanre Scheepers <21100387@virtualwindow.co.za>
2022-09-04 03:14:19 +0200

    FT Product Card

--- client/src/components/ProductCards.js
+++ client/src/components/ProductCards.js
@@ -6,33 +6,62 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import '../scss/productCards.scss';
-
+import { IconButton } from '@mui/material';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 // individual product cards
 const IndividualProductCard = (props) => {
 
 
     return (
         <div className='card'>
-            <Card sx={{ maxWidth: 300 }}>
-                <CardMedia
+            <Card >
+                <CardMedia className='card__intro__image'
                     component="img"
                     src={props.imgUrl}
                     alt={props.name}
-                    height="140" />
+                    height="160" />
                 <CardContent>
-                    <Typography gutterBottom variant="p" component="div">
+                    <Typography gutterBottom variant="p" className='card__featureitems__carousel__text'>
                         {props.name}
                     </Typography>
                     <Typography gutterBottom variant="p" component="div">
                         R {props.price}.00
                     </Typography>
+                    <Typography className='card__featureitems__carousel__text__textbody'>
+                        Shanre Scheepers - Buffalo
+                        <hr style={{ width: "100px" }} />
+                    </Typography>
                 </CardContent>
-                <CardActions>
-                    <Button size="small">Description</Button>
-                    <Button size="small">Product Information</Button>
-                </CardActions>
+                <IconButton>
+                    <ShoppingBasketIcon style={{ height: "50px", marginRight: "16px" }}></ShoppingBasketIcon>
+                </IconButton>
+                <Button variant="contained" href="#outlined-buttons" style={{ height: "50px", width: "150px", margin: "none", backgroundColor: "#B6AF93" }}  >
+                    DESCRIPTION
+                </Button>
             </Card>
         </div >
+        //      <Card >
+        //      <CardMedia>
+        //          <img src={buffalo} className="homepage__intro__image" style={{ height: "160px", margin: "none" }} />
+        //      </CardMedia>
+        //      <CardContent>
+        //          <Typography gutterBottom className='homepage__featureitems__carousel__text'>
+        //              Buffalo Cow
+        //          </Typography>
+        //          {/* PULLS */}
+        //          <Typography className='homepage__featureitems__carousel__text__textbody'>
+        //              Shanre Scheepers - Buffalo
+        //              <hr style={{ width: "100px" }} />
+        //              R 5000
+        //          </Typography>
+        //      </CardContent>
+        //      <IconButton>
+        //          <ShoppingBasketIcon style={{ height: "50px", marginRight: "16px" }}></ShoppingBasketIcon>
+        //      </IconButton>
+        //      <Button variant="contained" href="#outlined-buttons" style={{ height: "50px", width: "150px", margin: "none", backgroundColor: "#B6AF93" }}  >
+        //          SHOP PRINTS
+        //      </Button>
+        //  </Card>
 
     )
 }
